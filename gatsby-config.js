caac186494d7e50fe8bc4fca78425f9a336fd5cf
조상규/ThinQ 클라우드플랫폼개발팀 <sangkyu.cho@lge.com>
2025-05-11 15:23:40 +0900

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-code-notes',
-    description: `A starter for gatsby-theme-code-notes`,
-    author: 'Zander',
+    title: 'sangkyu.wernicke',
+    description: `sangkyu.wernicke`,
+    author: 'Sangkyu',
   },
   plugins: [
     {
