28a82824effba7a6edfc41c50ebca3ecf5f60a4c
sangkyuwernicke <154011107+sangkyuwernicke@users.noreply.github.com>
2026-07-23 13:53:43 +0900

    Create report.js

---
+++ pages/report.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ReportPage = () => {
+  return (
+    <main>
+      <h1>Paper Survey Report</h1>
+      <p>논문 연구 자료입니다.</p>
+    </main>
+  )
+}
+
+export default ReportPage
